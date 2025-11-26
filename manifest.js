391dcd82288ed0abd03c13ee2a9d3bee34756c39
toyro7799 <toyro7799@gmail.com>
2025-11-27 01:56:21 +0200

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+  "name": "الخباز الذكي",
+  "short_name": "الخباز",
+  "description": "تطبيق حاسبة المخبوزات الذكي للعمل بدون اتصال",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#ffc107",
+  "icons": [
+    {
+      "src": "./assets/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "./assets/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
